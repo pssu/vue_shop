8962536b6edaa131b11bce7e6aa006be5eacf5e8
pssu <1229232647@qq.com>
2020-03-11 11:59:59 +0800

    完成了登录功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
 
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
+
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
 Vue.use(Button)
+Vue.prototype.$message = Message
